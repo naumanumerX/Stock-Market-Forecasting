d3ee9aa0f13efe5daa6f526772041433cae4bec2
Nauman <naumanumer10@outlook.com>
2023-11-09 17:31:16 +0000

    User Can Search For Any London Stock

--- src/App.js
+++ src/App.js
@@ -3,14 +3,43 @@ import Navigation from './Components/Navigation';
 import HistoricalData from './Components/HistoricalData';
 import CandleStick from './Components/CandleStick';
 import { useState,useEffect } from 'react';
-
+import DataRangePicker from './Components/DataRangePicker';
+import { addDays ,differenceInDays} from 'date-fns'
  function App() {
   
 
   const [stockSymbol,setStockSymbol]=useState('BARC');
 
   const [stockData, setStockData] = useState(null);
-  
+
+  //for date range
+  const [customDays,setCustomDays]=useState(0);
+  const [range,setRange]=useState([
+    {
+        startDate:(new Date()),
+        endDate: addDays(new Date(),- 15),
+        key:'selection'
+    }
+  ]);
+
+  let daysDifference=0;
+
+useEffect(()=>{
+   daysDifference = differenceInDays(range[0].endDate, range[0].startDate); // use can see data data of any dates he selects
+   console.log(`Difference in days: ${daysDifference}`);
+   
+   let exculdingWeekends=Math.floor(daysDifference/7);
+   let additonalDays=Math.floor(daysDifference%7);
+   exculdingWeekends *=2;
+   exculdingWeekends -=2;
+   exculdingWeekends +=additonalDays;
+
+  daysDifference -=exculdingWeekends;
+
+   setCustomDays(daysDifference);
+
+},[range])
+
 
   return (
 
@@ -21,8 +50,10 @@ import { useState,useEffect } from 'react';
       <h1>App Componenrt renderered</h1>
      
   {stockSymbol}
-  <HistoricalData stockData={stockData} setStockData={setStockData} />
-<CandleStick  stockData={stockData} />
+  {customDays}
+  <DataRangePicker range={range} setRange={setRange} />
+  <HistoricalData  stockSymbol={stockSymbol} stockData={stockData} setStockData={setStockData} customDays={customDays} />
+<CandleStick  stockData={stockData} customDays={customDays}/>
 
       
         
