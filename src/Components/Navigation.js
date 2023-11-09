d35be3b634b442f2dbc07f90d95cc2da2eabe70f
Nauman <naumanumer10@outlook.com>
2023-11-09 17:43:18 +0000

    Feature: User Can Enter Any Stock Symbol

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,47 +1,53 @@
-import React, { useState ,useEffect} from 'react';
-import './Navigation.css';
-import HistoricalData from './HistoricalData';
-const Navigation = ({stockSymbol,setStockSymbol}) => {
+import React, { useState, useEffect } from "react";
+import "./Navigation.css";
+import HistoricalData from "./HistoricalData";
+const Navigation = ({ stockSymbol, setStockSymbol }) => {
+  const [stockSymbolValue, setStockSymbolValue] = useState("");
+  const handleChange = (e) => {
+    // const newSymbol = e.target.value;
 
-  
-    const handleChange = (e) => {
-        const newSymbol = e.target.value;
-    
-        // Use a setTimeout to update the stockSymbol state after a delay
-        setTimeout(() => {
-          setStockSymbol(newSymbol);
-        }, 3000);
-      };
+    // Use a setTimeout to update the stockSymbol state after a delay
+    // setTimeout(() => {
+    setStockSymbolValue(e.target.value);
+    // }, 3000);
+  };
 
   return (
-<>
-    <nav className="navbar">
-      <div className="left-section">
-       
-          MDX Stocks
-        
-      </div>
-      <div className="search-section">
-        <input type="text" placeholder="Enter Stock Name or Code" className="search-input"  />
-        <button className="search-button" onClick={(e)=>handleChange(e)}>Search Stock</button>
-      </div>
-      <div className="right-section">
-        <a>Historical Data</a>
-        <a href="/about">About</a>
-        <a href="/charts">Charts</a>
-        <a href="/signup">Sign Up/Login</a>
-
-        
-      </div>
-    </nav>
-    <h3>{stockSymbol}</h3>
-    
-    
-   
-
-
+    <>
+      <nav className="navbar">
+        <div className="left-section">MDX Stocks</div>
+        <div className="search-section">
+          <input
+            type="text"
+            placeholder="Enter Stock Name or Code"
+            className="search-input"
+            onChange={handleChange}
+            value={stockSymbolValue}
+          />
+          <button
+            className="search-button"
+            onClick={() => {
+              setStockSymbol(stockSymbolValue);
+            }}
+          >
+            Search Stock
+          </button>
+        </div>
+        <div className="right-section">
+          <a href="/historical-data">Historical Data</a>
+          <a href="/about">About</a>
+          <a href="/charts">Charts</a>
+          <a href="/signup">Sign Up/Login</a>
+        </div>
+      </nav>
+      <h3
+        style={{
+          textTransform: "uppercase",
+        }}
+      >
+        {stockSymbol}
+      </h3>
     </>
-    
   );
 };
 
