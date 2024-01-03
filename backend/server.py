e3b0142c75ec3acc451f5eebbfea1c4c84d7ef11
Nauman <naumanumer10@outlook.com>
2024-01-03 16:58:51 +0000

    Added New Route

--- backend/server.py
+++ backend/server.py
@@ -3,18 +3,18 @@ from flask_cors import CORS
 import asyncio
 
 from model import run_ml_algorithm
-
+from about import run_about
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
 ml_result = ''
-
-@app.route('/abc', methods=['POST'])
+stock_symbol=''
+@app.route('/fetchData', methods=['POST'])
 def handle_input():
-    global ml_result
+    global ml_result,stock_symbol
     if request.method == 'POST':
         data = request.get_json()
-        stock_symbol = data.get('stock_symbol', '')
+        stock_symbol= data.get('stock_symbol', '')
         print(f'Received stock symbol: {stock_symbol}')
 
         # Call your ML algorithm
@@ -24,7 +24,7 @@ def handle_input():
     else:
         return jsonify({'message': 'Method Not Allowed'}), 405
 
-@app.route('/abcd', methods=['POST'])
+@app.route('/getPrediction', methods=['POST'])
 def algorithm_response():
     global ml_result
     if request.method == 'POST':
@@ -32,11 +32,25 @@ def algorithm_response():
         stock_symbol = data.get('stock_symbol', '')
 
         # Print the stored ml_result
-        print("kachay katch de kaadan", ml_result)
+        print("Ml Result", ml_result)
 
         return jsonify({'stock_symbol': stock_symbol, 'ml_result': ml_result})
     else:
         return jsonify({'message': 'Method Not Allowed'}), 405
+@app.route('/about', methods=['POST'])
+def about():
+    global stock_symbol
+    if request.method == 'POST':
+        print(f'Received stock symbol: {stock_symbol}')
+        data = request.get_json()
+        stock_symbol = stock_symbol
+        
 
+        about = run_about(stock_symbol)
+        print(about)
+        # about = {'about': "abc"}
+        return jsonify({ 'about': about})
+    else:
+        return jsonify({'message': 'Method Not Allowed'}), 405
 if __name__ == '__main__':
     app.run(debug=True)
