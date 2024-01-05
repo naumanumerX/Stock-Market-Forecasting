cc2b2bd3dd170b4d5aec3ace99c27b0a4e05a8a9
Nauman <naumanumer10@outlook.com>
2024-01-05 20:54:24 +0000

    feat: Adding TimeSeries Chart

--- src/Charts/zoomableTimeSeriesData.js
+++ src/Charts/zoomableTimeSeriesData.js
@@ -1,59 +1,106 @@
-import React, { useMemo } from 'react';
-import ReactApexChart  from "react-apexcharts";
+import React from 'react';
+import ReactApexChart from 'react-apexcharts';
+import dayjs from 'dayjs';
 
-const zoomableTimeSeriesData = ({ stockData,stockSymbol }) => {
 
-    const TimeSeries=[];
+const TimeSeriesChart = ({ stockData, stockSymbol, customDays }) => {
+  const timeSeriesData = [];
+
   if (!stockData) {
-    return (
-      <div>
-        Stock data is not available.
-      </div>
-    );
+    return <div>Stock data is not available.</div>;
   }
 
-  
-  Object.entries(stockData).slice(0,30).map(([date,value]) => {
-   // console.log("data is ",date ,"values is ",value["1. open"]);
-   TimeSeries.push({
-        x:date,
-       y:  value["2. high"],
-      
-    
-            
-    })
-   // useMemo(()=>candleStickData,stockData) 
-    return null; 
-  }
-  
-  
-  );
+  Object.entries(stockData)
+    .slice(0, customDays)
+    .forEach(([date, value]) => {
+      timeSeriesData.push({
+        x: new Date(date).getTime(),
+        y: [value['1. open'], value['2. high'], value['3. low'], value['4. close']],
+      });
+    });
 
- 
+  const options = {
+    chart: {
+      type: 'area',
+      stacked: false,
+      height: 350,
+      zoom: {
+        type: 'x',
+        enabled: true,
+        autoScaleYaxis: true,
+      },
+      toolbar: {
+        autoSelected: 'zoom',
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    markers: {
+      size: 0,
+    },
+    title: {
+      text: `Time Series Chart - ${stockSymbol}`,
+      align: 'left',
+    },
+    fill: {
+      type: 'gradient',
+      gradient: {
+        shadeIntensity: 1,
+        inverseColors: false,
+        opacityFrom: 0.5,
+        opacityTo: 0,
+        stops: [0, 90, 100],
+      },
+    },
+    yaxis: {
+      labels: {
+        formatter: function (val) {
+          return val.toFixed(2);
+        },
+      },
+      title: {
+        text: 'Price',
+      },
+    },
+    xaxis: {
+      type: 'datetime',
+      labels: {
+        formatter: function (val) {
+          return dayjs(val).format('MMM DD HH:mm');
+        },
+      },
+    },
+    tooltip: {
+      shared: false,
+      y: {
+        formatter: function (val) {
+          return val.toFixed(2);
+        },
+      },
+    },
+  };
+
   return (
-    <>
-     
-      
+    <div className="chart-component">
+      <h3 style={{ textAlign: 'center' }}>
+        <u>Time Series Chart</u>
+      </h3>
 
-
-
-
- <ReactApexChart 
-      series ={
-
-        [
+      <div className="chart">
+        <ReactApexChart
+          series={[
             {
-                data:zoomableTimeSeries
-            }
-        ]
-      }
-      
-      options={zoomableTimeSeries(stockSymbol)}
-      type="area"
-      width="100%" height="50%"
-      /> 
-    </>
+              data: timeSeriesData,
+            },
+          ]}
+          options={options}
+          type="area"
+          style={{ width: '100%', height: '100%' }}
+        />
+      </div>
+    </div>
   );
-}
+};
 
-export default zoomableTimeSeriesData;
+export default TimeSeriesChart;
